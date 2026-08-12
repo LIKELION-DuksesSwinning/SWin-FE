f1e772a3642137914b60a3a335088af3305828c7
stopwonee <stopw0n@naver.com>
2026-08-13 00:25:55 +0900

    chore: App.jsx 설정 변경(/ as login, /home as home)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Login from './pages/Login/Login.jsx';
+
 import BottomNav from './components/BottomNav/BottomNav.jsx';
 import ReservationDate from './pages/Clinic/ReservationDate.jsx';
 import ReservationTime from './pages/Clinic/ReservationTime.jsx';
@@ -6,28 +8,34 @@ import ReservationComplete from './pages/Clinic/ReservationComplete.jsx';
 import ReservationHistory from './pages/Clinic/ReservationHistory.jsx';
 import WeeklyCalendar from './components/WeeklyCalendar/WeeklyCalendar.jsx';
 import PoolSearch from './pages/Pool/PoolSearch.jsx';
-import './App.css'; 
+import './App.css';
 
-// const Home = () => <div>홈 화면입니다</div>;
 const Home = () => (
   <div>
     <WeeklyCalendar />
   </div>
 );
+
 const Analysis = () => <div>분석 화면입니다</div>;
-const Pool = () => <div>수영장 화면입니다</div>;
-const Clinic = () => <div>클리닉 화면입니다</div>;
 const My = () => <div>마이 화면입니다</div>;
 
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/';
+
   return (
     <div className="app-container">
       <div className="content-area">
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* 처음 접속하는 온보딩/로그인 화면 */}
+          <Route path="/" element={<Login />} />
+
+          {/* 로그인 성공 후 이동할 홈 화면 */}
+          <Route path="/home" element={<Home />} />
+
           <Route path="/analysis" element={<Analysis />} />
           <Route path="/pool" element={<PoolSearch />} />
-          <Route path="/clinic" element={<ReservationDate />} />  
+          <Route path="/clinic" element={<ReservationDate />} />
           <Route path="/clinic/time" element={<ReservationTime />} />
           <Route path="/clinic/complete" element={<ReservationComplete />} />
           <Route path="/clinic/history" element={<ReservationHistory />} />
@@ -35,9 +43,10 @@ function App() {
         </Routes>
       </div>
 
-      <BottomNav />
+      {/* 로그인 화면에서는 하단 네비게이션을 숨김 */}
+      {!isLoginPage && <BottomNav />}
     </div>
-  )
+  );
 }
 
 export default App;
